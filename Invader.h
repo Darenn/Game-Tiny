90717de5d909e44eff7ada28c13d7f8a3d544748
Darenn <keller.darenn@gmail.com>
2021-01-12 00:03:21 +0100

    added the movement that changes with killing (with a bug to fix)
    Need to make a new implementation.

--- Invader.h
+++ Invader.h
@@ -13,6 +13,13 @@ class Invader {
     SPRITE sprite;
     bool isDead;
 
+    Invader() {
+      sprite = ssd1306_createSprite(0, 0, sizeof(heartImage),  heartImage);
+      sprite.x = 0;
+      sprite.y = 0;
+      isDead = false;
+    }
+
     Invader(int type, int posX, int posY) {
       sprite = ssd1306_createSprite(posX, posY, sizeof(heartImage),  heartImage);
       sprite.x = posX;
@@ -23,11 +30,16 @@ class Invader {
     void draw() {
       sprite.eraseTrace();
       sprite.draw();
+      //ssd1306_drawSpriteEx(sprite.x, sprite.y, sizeof(heartImage),  heartImage);
     }
 
     void kill() {
       isDead = true;
       sprite.erase();
+    }
+
+    void getPosX(uint_fast8_t idx, uint_fast8_t turn) {
+      return idx*10 + turn*10;
     }
 };
 
--- game_tiny.ino
+++ game_tiny.ino
@@ -1,15 +1,15 @@
 /**
- *   Attiny85 PINS
- *             ____
- *   RESET   -|_|  |- 3V
- *   SCL (3) -|    |- (2)
- *   SDA (4) -|    |- (1) HP
- *   GND     -|____|- (0) INPUT
- *
- *   Atmega328 PINS: connect LCD to A4/A5
- */
+     Attiny85 PINS
+               ____
+     RESET   -|_|  |- 3V
+     SCL (3) -|    |- (2)
+     SDA (4) -|    |- (1) HP
+     GND     -|____|- (0) INPUT
 
-#include "ssd1306.h" 
+     Atmega328 PINS: connect LCD to A4/A5
+*/
+
+#include "ssd1306.h"
 #include "graphics.h"
 #include "input.h"
 #include "audio.h"
@@ -23,14 +23,16 @@
 
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define INVADERS_COUNT_PER_ROW 3
-#define INVADERS_ROW_COUNT 5
+#define INVADERS_COUNT_PER_ROW 10
+#define INVADERS_ROW_COUNT 2
 #define INVADERS_COUNT INVADERS_COUNT_PER_ROW * INVADERS_ROW_COUNT
-#define INVADERS_X_MOVE_COUNT 15 // How many time invaders moves on X axis before going down
 #define TIME_PER_FRAME 32 // in ms
+#define INVADER_WIDTH 8
+
+int invaderXMoveCount = 15; // How many time invaders moves on X axis before going down
 
 Player* p = playerCreate();
-Invader* invaders [INVADERS_COUNT];
+Invader invaders [INVADERS_COUNT];
 Bullet theBullet;
 
 void setup() {
@@ -41,15 +43,15 @@ void setup() {
   //setup_inputs();
   DDRB |= 0b00011010;         // set PB1 as output (for the speaker), PB0 and PB2 as input
   //sei();                      // enable all interrupts*/ //TODO more optimized than pinmode?
-    
+
   ssd1306_clearScreen( );
 
-  for(uint_fast8_t y = 0; y < INVADERS_ROW_COUNT; ++y) {
-    for(uint_fast8_t x = 0; x < INVADERS_COUNT_PER_ROW; ++x) {
-       invaders[x + y * INVADERS_COUNT_PER_ROW] = new Invader(INVADER_0,10 * x, 10 * y);
-       invaders[x + y * INVADERS_COUNT_PER_ROW]->draw();
+  for (uint_fast8_t y = 0; y < INVADERS_ROW_COUNT; ++y) {
+    for (uint_fast8_t x = 0; x < INVADERS_COUNT_PER_ROW; ++x) {
+      invaders[x + y * INVADERS_COUNT_PER_ROW] = Invader(INVADER_0, 10 * x, 10 * y);
+      invaders[x + y * INVADERS_COUNT_PER_ROW].draw();
     }
-  } 
+  }
 
   theBullet.sprite = ssd1306_createSprite(0, 0, sizeof(shootSprite),  shootSprite);
   theBullet.enabled = false;
@@ -58,57 +60,101 @@ void setup() {
 }
 
 uint_fast8_t invaderCounter = 0; // On each invade call we move only one row for performance
+static signed int invaderDirection = 1; // 1 = right and -1 = left
+static uint_fast8_t nextInvaderXMoveCount = invaderXMoveCount;
+static uint_fast8_t invaderXSpeed = 2;
 
 static void invade() {
-  static unsigned long invaderTimerInterval = 2000; // between each movement in milisecond
+  static unsigned long invaderTimerInterval = 100; // between each movement in milisecond
   static unsigned long lastInvaderMoveTime = 0;
   static uint_fast8_t invasionXmoveCounter = 0;
-  static uint_fast8_t invaderXSpeed = 2;
-  static uint_fast8_t invaderDirection = 1; // 1 = right and -1 = left
+  
+  
   static uint_fast8_t invaderYSpeed = 3;
 
-  if(millis() - lastInvaderMoveTime > invaderTimerInterval) {
+  if (millis() - lastInvaderMoveTime > invaderTimerInterval) {
     lastInvaderMoveTime = millis();
-    if(invasionXmoveCounter >= INVADERS_X_MOVE_COUNT) {
+    if (invasionXmoveCounter >= invaderXMoveCount) {
       invaderDirection = -invaderDirection;
       invasionXmoveCounter = 0;
-      for(uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
-        if(!invaders[i]->isDead) {
-          invaders[i]->sprite.y += invaderYSpeed;
-          invaders[i]->draw();
+      invaderXMoveCount = nextInvaderXMoveCount;
+      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
+        if (!invaders[i].isDead) {
+          invaders[i].sprite.y += invaderYSpeed;
+          invaders[i].draw();
         }
-      }  
+      }
     } else {
       ++invasionXmoveCounter;
-      for(uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
-        if(!invaders[i]->isDead) {
-          invaders[i]->sprite.x += invaderXSpeed * invaderDirection;
-          invaders[i]->draw(); 
+      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
+        if (!invaders[i].isDead) {
+          invaders[i].sprite.x += invaderXSpeed * invaderDirection;
+          invaders[i].draw();
         }
-      }  
+      }
     }
   }
 }
 
+int getColumnWithIndex(int index) {
+  return index % INVADERS_COUNT_PER_ROW;
+}
+
+
+
 void loop() {
   float startLoopTime = millis();  // Save time to get 30 FPS
   note(0, 0);
-  
+
   playerUpdate(p);
 
-  if(theBullet.enabled) {
-    for(uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
-     if(!invaders[i]->isDead && isColliding(&theBullet.sprite,&invaders[i]->sprite)) {
+  if (theBullet.enabled) {
+    for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
+      if (!invaders[i].isDead && isColliding(&theBullet.sprite, &invaders[i].sprite)) {
         kill(&theBullet);
-        invaders[i]->kill();
+        invaders[i].kill();
+        // TODO optimize by not making anything if survivor on same column AND do not double check de column
+
+        // Check if there is a survivor on the LEFT side or same column, move farther if not
+        bool shouldMoveFarther = true;
+        uint_fast8_t invaderColumn = i % INVADERS_COUNT_PER_ROW;
+        for (uint_fast8_t col = 0; col <= invaderColumn; ++col) {
+          for (i = 0; i < INVADERS_ROW_COUNT; ++i) {
+            if (!invaders[col + i*INVADERS_COUNT_PER_ROW].isDead) {
+              shouldMoveFarther = false;
+              break;
+            }
+          }
+        }
+        if (shouldMoveFarther) {
+          // If invaders are moving to the left we directly incr the xMoveCount, otherwise we will incr it next time there is a direction change.
+          nextInvaderXMoveCount += INVADER_WIDTH/invaderXSpeed;
+          if(invaderDirection < 0) invaderXMoveCount += INVADER_WIDTH/invaderXSpeed;
+        } else {
+          // Check if there is a survivor on the RIGHT side or same column, move farther if not
+          shouldMoveFarther = true;
+          for (int col = invaderColumn; col < INVADERS_COUNT_PER_ROW; ++col) {
+            for (i = 0; i < INVADERS_ROW_COUNT; ++i) {
+              if (!invaders[col + i * INVADERS_COUNT_PER_ROW].isDead) {
+                shouldMoveFarther = false;
+                break;
+              }
+            }
+          }
+          if (shouldMoveFarther) {
+            // If invaders are moving to the RIGHT we directly incr the xMoveCount, otherwise we will incr it next time there is a direction change.
+            nextInvaderXMoveCount += INVADER_WIDTH/invaderXSpeed;
+            if(invaderDirection > 0) invaderXMoveCount  += INVADER_WIDTH/invaderXSpeed;
+          }
+        }
         break;
-     }
+      }
     }
     bulletUpdate(&theBullet);
     bulletDraw(&theBullet);
   }
 
-  //invade();
+  invade();
 
   if (getButtonAPressed()) {
     theBullet.enabled = true;
@@ -116,19 +162,20 @@ void loop() {
     theBullet.sprite.y = p->sprite.y - 5;
   }
 
-   playerDraw(p);
-  
+  playerDraw(p);
+
   // Fix 30 FPS
   signed int timeToWait = TIME_PER_FRAME - (millis() - startLoopTime);
-  if(timeToWait > 0) {
+  if (timeToWait > 0) {
     delay(timeToWait);
   }
 
   // DEBUG
-  debugDisplayInt(freeMemory(), 0, 50);
+  debugDisplayInt(invaderXMoveCount, 0, 50);
+  debugDisplayInt(invaderDirection, 100, 50);
+  //debugDisplayInt(freeMemory(), 0, 50);
   //debugDisplayInt(timeToWait, 0, 0);
   //uint_fast16_t padPinValue = analogRead(PIN_DPAD);
   //debugDisplayInt(padPinValue, 40, 0);
   //debugDisplayInt(digitalRead(PIN_BUTTON_A), 70, 0);
-
 }
