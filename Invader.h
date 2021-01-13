e523f348df5b326f0bdddb4e298089393a533a17
Darenn <keller.darenn@gmail.com>
2021-01-13 10:25:48 +0100

    Will try smth stupid.

--- Invader.h
+++ Invader.h
@@ -7,8 +7,21 @@
 #define INVADER_1 1
 #define INVADER_2 2
 
+#define XSPACE_BETWEEN_INVADERS 10
+#define YSPACE_BETWEEN_INVADERS_ROWS 10
+#define INVADER_STRAFE_MOVE_DIST 2
+#define INVADER_DIVE_MOVE_DIST 2
+#define INVADERS_COUNT_PER_ROW 5 // TODO rename column
+#define STARTING_INVADER_X_MOVE_COUNT 15 + 15
+#define INVADERS_ROW_COUNT 3
+#define INVADERS_COUNT INVADERS_COUNT_PER_ROW * INVADERS_ROW_COUNT
+#define INVADER_WIDTH 8
+
+uint_fast8_t invaderXSpeed = 2; // TODO wont change should be define
+signed int invaderDirection = 1; // 1 = right and -1 = left
+
 class Invader {
-  
+
   public:
     SPRITE sprite;
     bool isDead;
@@ -27,20 +40,185 @@ class Invader {
       isDead = false;
     }
 
-    void draw() {
-      sprite.eraseTrace();
-      sprite.draw();
-      //ssd1306_drawSpriteEx(sprite.x, sprite.y, sizeof(heartImage),  heartImage);
-    }
+    
 
     void kill() {
       isDead = true;
       sprite.erase();
     }
-
-    void getPosX(uint_fast8_t idx, uint_fast8_t turn) {
-      return idx*10 + turn*10;
-    }
 };
+
+Invader invaders [INVADERS_COUNT];
+
+
+int getIndexByCoordinates(int row, int col) {
+  return col + row * INVADERS_COUNT_PER_ROW;
+}
+
+int invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT; // How many time invaders moves on X axis before going down
+
+int getColumnWithInvaderIndex(int index) {
+  return index % INVADERS_COUNT_PER_ROW;
+}
+
+int getRowWithInvaderIndex(int index) {
+  return index / INVADERS_COUNT_PER_ROW;
+}
+
+
+uint_fast8_t getFirstInvaderAliveCol() {
+  for (uint_fast8_t col = 0; col < INVADERS_COUNT_PER_ROW; ++col) {
+    for (uint_fast8_t row = 0; row < INVADERS_ROW_COUNT; ++row) {
+      if (!invaders[getIndexByCoordinates(row, col)].isDead) {
+        return col;
+      }
+    }
+  }
+  return 99;
+}
+
+uint_fast8_t getLastInvaderAliveCol() {
+  for (uint_fast8_t col = INVADERS_COUNT_PER_ROW - 1; col > 0; --col) {
+    for (uint_fast8_t row = 0; row < INVADERS_ROW_COUNT; ++row) {
+      if (!invaders[getIndexByCoordinates(row, col)].isDead) {
+        return col;
+      }
+    }
+  }
+  return 99;
+}
+
+// TODO two functions
+void compensateDead(bool isDiving) {
+  uint_fast8_t leftiestAliveColumn = getColumnWithInvaderIndex(getFirstInvaderAliveCol());
+  uint_fast8_t leftCompensation = leftiestAliveColumn * (INVADER_WIDTH / invaderXSpeed);
+  uint_fast8_t rightiestAliveColumn = getColumnWithInvaderIndex(getLastInvaderAliveCol());
+  uint_fast8_t rightCompensation = (INVADERS_COUNT_PER_ROW - 1 - rightiestAliveColumn) * (INVADER_WIDTH / invaderXSpeed);
+
+  static uint_fast8_t oldRightCompensation = 0;
+  static uint_fast8_t oldLeftCompensation = 0;
+
+  if (isDiving) {
+    oldRightCompensation = rightCompensation;
+    oldLeftCompensation = leftCompensation;
+    invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + rightCompensation + leftCompensation;
+    return;
+  }
+
+  // If we are moving in the direction, apply the new compensation right now otherwise at next dive
+  if (invaderDirection > 0) { // moving to right
+#ifdef DEBUG
+    attinyAssert(oldRightCompensation <= rightCompensation, "!: oldRComp <= rComp"); // Should never happen as you cannot revive aliens during game
+#endif
+    if (rightCompensation > oldRightCompensation) {
+      invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + rightCompensation - oldRightCompensation + oldLeftCompensation;
+    }
+
+  } else { // invaderDirection < 0 // moving to left
+#ifdef DEBUG
+    attinyAssert(oldLeftCompensation <= leftCompensation, "!: oldLComp <= lComp"); // Should never happen as you cannot revive aliens during game
+#endif
+    if (leftCompensation > oldLeftCompensation) {
+      invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + leftCompensation - oldLeftCompensation + oldRightCompensation;
+    }
+  }
+}
+
+uint_fast8_t invaderCounter = 0; // TODO On each invade call we move only one row for performance
+
+static uint_fast8_t nextInvaderXMoveCount = invaderXMoveCount;
+
+
+int getPosX(uint_fast8_t idx, uint_fast8_t strafeCounter, uint_fast8_t diveCounter) {
+
+  static uint_fast8_t oldRightStrafeCounter = 0;
+  int posXStart = getColumnWithInvaderIndex(idx) * XSPACE_BETWEEN_INVADERS;
+  int offsetXStrafeRight = strafeCounter * INVADER_STRAFE_MOVE_DIST;
+  int offsetXStrafeLeft = oldRightStrafeCounter*INVADER_STRAFE_MOVE_DIST - strafeCounter*INVADER_STRAFE_MOVE_DIST;
+
+  if(diveCounter % 2 == 0) { // if we are moving to right
+    oldRightStrafeCounter = strafeCounter;
+    return posXStart + offsetXStrafeRight;
+  } else {
+    return posXStart + offsetXStrafeLeft;
+  }
+  
+  /*
+  uint_fast8_t leftiestAliveColumn = getColumnWithInvaderIndex(getFirstInvaderAliveCol());
+  uint_fast8_t leftCompensation = leftiestAliveColumn * (INVADER_WIDTH / invaderXSpeed);
+  uint_fast8_t rightiestAliveColumn = getColumnWithInvaderIndex(getLastInvaderAliveCol());
+  uint_fast8_t rightCompensation = (INVADERS_COUNT_PER_ROW - 1 - rightiestAliveColumn) * (INVADER_WIDTH / invaderXSpeed);
+  if(diveCounter % 2 == 0) {
+    oldRightStrafeCounter=strafeCounter;
+    return  getColumnWithInvaderIndex(idx) * XSPACE_BETWEEN_INVADERS + leftCompensation * INVADER_STRAFE_MOVE_DIST - rightCompensation * INVADER_STRAFE_MOVE_DIST + strafeCounter * INVADER_STRAFE_MOVE_DIST;
+  } else {
+    return getColumnWithInvaderIndex(idx) * XSPACE_BETWEEN_INVADERS + leftCompensation * INVADER_STRAFE_MOVE_DIST - rightCompensation * INVADER_STRAFE_MOVE_DIST - strafeCounter * INVADER_STRAFE_MOVE_DIST + oldRightStrafeCounter*INVADER_STRAFE_MOVE_DIST;
+  }*/
+
+  //!!!!!!!!!!!! COME back to start need to reverse
+
+  /*static int diff = 0;
+  if(diveCounter % 2 == 0) {
+    debugDisplayInt(oldRightStrafeCounter, 0, 30);
+    debugDisplayInt(diff, 0, 50);
+    oldRightStrafeCounter=strafeCounter;
+    return getColumnWithInvaderIndex(idx)*XSPACE_BETWEEN_INVADERS + strafeCounter*INVADER_STRAFE_MOVE_DIST - diff*INVADER_STRAFE_MOVE_DIST;
+    } else {
+    diff = abs(strafeCounter-oldRightStrafeCounter);
+    return getColumnWithInvaderIndex(idx)*XSPACE_BETWEEN_INVADERS + oldRightStrafeCounter*INVADER_STRAFE_MOVE_DIST - strafeCounter*INVADER_STRAFE_MOVE_DIST;
+    }*/
+
+}
+
+int getPosY(uint_fast8_t idx, uint_fast8_t diveCounter) {
+  return getRowWithInvaderIndex(idx) * YSPACE_BETWEEN_INVADERS_ROWS + diveCounter * INVADER_DIVE_MOVE_DIST;
+}
+
+void drawInvader(Invader* i, int index, int strafeCounter, int diveCounter) {
+      //i->sprite.x = getPosX(index, strafeCounter, diveCounter);
+      //i->sprite.y = getPosY(index, diveCounter);
+      i->sprite.eraseTrace();
+      i->sprite.draw();
+      //ssd1306_drawSpriteEx(getPosX(index, strafeCounter), getPosY(index, diveCounter), sizeof(heartImage),  heartImage); //!!!!! y is in blocks vertical position in blocks (pixels/8)
+    }
+
+static void invade() { // TODO should let the draw to somehting else
+  static unsigned long invaderTimerInterval = 500; // between each movement in milisecond
+  static unsigned long lastInvaderMoveTime = 0;
+  static uint_fast8_t invasionXmoveCounter = 0; // TODO rename STRAFE_COUNTER
+  static uint_fast8_t diveCounter = 0;
+
+
+  static uint_fast8_t invaderYSpeed = 3;
+
+  if (millis() - lastInvaderMoveTime > invaderTimerInterval) {
+    lastInvaderMoveTime = millis();
+    if (invasionXmoveCounter >= invaderXMoveCount) { // DIVE
+      ++diveCounter;
+      compensateDead(true);
+      invaderDirection = -invaderDirection;
+      invasionXmoveCounter = 0;
+      //invaderXMoveCount = nextInvaderXMoveCount;
+      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
+        if (!invaders[i].isDead) {
+          invaders[i].sprite.y += invaderYSpeed;
+          drawInvader(&invaders[i],i, invasionXmoveCounter, diveCounter);// TODO wrong neeed another counter for Y
+        }
+      }
+    } else {
+      ++invasionXmoveCounter;
+      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
+        if (!invaders[i].isDead) {
+          invaders[i].sprite.x += invaderXSpeed * invaderDirection;
+          drawInvader(&invaders[i],i, invasionXmoveCounter, diveCounter);// TODO wrong neeed another counter for Y
+        }
+      }
+    }
+  }
+  debugDisplayInt(invasionXmoveCounter, 0, 80);
+}
+
+
+
 
 #endif
--- game_tiny.ino
+++ game_tiny.ino
@@ -23,17 +23,13 @@
 
 #define SCREEN_WIDTH 128 // OLED display width,  in pixels
 #define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define INVADERS_COUNT_PER_ROW 5 // TODO rename column
-#define INVADERS_ROW_COUNT 3
-#define INVADERS_COUNT INVADERS_COUNT_PER_ROW * INVADERS_ROW_COUNT
-#define TIME_PER_FRAME 32 // in ms
-#define INVADER_WIDTH 8
-#define STARTING_INVADER_X_MOVE_COUNT 15 + 15
 
-int invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT; // How many time invaders moves on X axis before going down
+
+#define TIME_PER_FRAME 32 // in ms
+
+
 
 Player* p = playerCreate();
-Invader invaders [INVADERS_COUNT];
 Bullet theBullet;
 
 void setup() {
@@ -50,7 +46,7 @@ void setup() {
   for (uint_fast8_t y = 0; y < INVADERS_ROW_COUNT; ++y) {
     for (uint_fast8_t x = 0; x < INVADERS_COUNT_PER_ROW; ++x) {
       invaders[x + y * INVADERS_COUNT_PER_ROW] = Invader(INVADER_0, 10 * x, 10 * y);
-      invaders[x + y * INVADERS_COUNT_PER_ROW].draw();
+      drawInvader(&invaders[x + y * INVADERS_COUNT_PER_ROW], x + y * INVADERS_COUNT_PER_ROW , 0, 0);
     }
   }
 
@@ -60,128 +56,7 @@ void setup() {
   playerForceDraw(p);
 }
 
-uint_fast8_t invaderCounter = 0; // On each invade call we move only one row for performance
-static signed int invaderDirection = 1; // 1 = right and -1 = left
-static uint_fast8_t nextInvaderXMoveCount = invaderXMoveCount;
-static uint_fast8_t invaderXSpeed = 2; // TODO wont change should be define
 
-static void invade() {
-  static unsigned long invaderTimerInterval = 1000; // between each movement in milisecond
-  static unsigned long lastInvaderMoveTime = 0;
-  static uint_fast8_t invasionXmoveCounter = 0;
-  
-  
-  static uint_fast8_t invaderYSpeed = 3;
-
-  if (millis() - lastInvaderMoveTime > invaderTimerInterval) {
-    lastInvaderMoveTime = millis();   
-    if (invasionXmoveCounter >= invaderXMoveCount) { // DIVE
-      compensateDead(true);
-      invaderDirection = -invaderDirection;
-      invasionXmoveCounter = 0;
-      //invaderXMoveCount = nextInvaderXMoveCount;
-      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
-        if (!invaders[i].isDead) {
-          invaders[i].sprite.y += invaderYSpeed;
-          invaders[i].draw();
-        }
-      }
-    } else {
-      ++invasionXmoveCounter;
-      for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
-        if (!invaders[i].isDead) {
-          invaders[i].sprite.x += invaderXSpeed * invaderDirection;
-          invaders[i].draw();
-        }
-      }
-    }
-  }
-  debugDisplayInt(invasionXmoveCounter, 0, 80);
-}
-
-
-int getColumnWithInvaderIndex(int index) {
-  return index % INVADERS_COUNT_PER_ROW;
-}
-
-int getIndexByCoordinates(int row, int col) {
-  return col + row*INVADERS_COUNT_PER_ROW;
-}
-
-uint_fast8_t getFirstInvaderAliveCol() {
-  for(uint_fast8_t col = 0; col < INVADERS_COUNT_PER_ROW; ++col) {
-    for(uint_fast8_t row = 0; row < INVADERS_ROW_COUNT; ++row) {
-      if(!invaders[getIndexByCoordinates(row, col)].isDead) {
-        return col;
-      }
-    }
-  }
-  return 99;
-}
-
-uint_fast8_t getLastInvaderAliveCol() {
-  for(uint_fast8_t col = INVADERS_COUNT_PER_ROW-1; col > 0; --col) {
-    for(uint_fast8_t row = 0; row < INVADERS_ROW_COUNT; ++row) {
-      if(!invaders[getIndexByCoordinates(row, col)].isDead) {
-        return col;
-      }
-    }
-  }
-  return 99;
-}
-
-// TODO two functions
-void compensateDead(bool isDiving) {
-  uint_fast8_t leftiestAliveColumn = getColumnWithInvaderIndex(getFirstInvaderAliveCol());
-  uint_fast8_t leftCompensation = leftiestAliveColumn*(INVADER_WIDTH/invaderXSpeed);
-  uint_fast8_t rightiestAliveColumn = getColumnWithInvaderIndex(getLastInvaderAliveCol());
-  uint_fast8_t rightCompensation = (INVADERS_COUNT_PER_ROW-1-rightiestAliveColumn)*(INVADER_WIDTH/invaderXSpeed);
-
-  static uint_fast8_t oldRightCompensation = 0;
-  static uint_fast8_t oldLeftCompensation = 0;
-
-  debugDisplayInt(leftCompensation/4, 40, 50);
-  debugDisplayInt(rightCompensation/4, 80, 50);
-
-  if(isDiving) {
-    oldRightCompensation = rightCompensation;
-    oldLeftCompensation = leftCompensation;
-    invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + rightCompensation + leftCompensation;
-    delay(1000);
-    return;
-  }
-
-  // If we are moving in the direction, apply the new compensation right now otherwise at next dive
-  if(invaderDirection > 0) { // moving to right
-    #ifdef DEBUG 
-      attinyAssert(oldRightCompensation<=rightCompensation, "!: oldRComp <= rComp");// Should never happen as you cannot revive aliens during game
-    #endif 
-    if(rightCompensation>oldRightCompensation) {
-        invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + rightCompensation - oldRightCompensation + oldLeftCompensation;
-        delay(1000);
-    } /*else { // oldRightCompensation==rightCompensation
-        invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + rightCompensation + oldLeftCompensation;
-    }*/
-    //nextInvaderXMoveCount = invaderXMoveCount + leftCompensation - oldLeftCompensation;
-    
-  } else { // invaderDirection < 0 // moving to left
-    #ifdef DEBUG 
-      attinyAssert(oldLeftCompensation<=leftCompensation, "!: oldLComp <= lComp"); // Should never happen as you cannot revive aliens during game
-    #endif 
-    if(leftCompensation>oldLeftCompensation) {
-        invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + leftCompensation - oldLeftCompensation + oldRightCompensation;
-        delay(1000);
-    } /*else { // oldLeftCompensation==leftCompensation
-        invaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + leftCompensation + oldRightCompensation;
-    }*/
-    //nextInvaderXMoveCount = invaderXMoveCount + rightCompensation - oldRightCompensation;
-  }
-
-
-
- // nextInvaderXMoveCount = STARTING_INVADER_X_MOVE_COUNT + leftCompensation + rightCompensation; // adding the other compensation at next dive
-}
-  
 
 
 
@@ -190,7 +65,7 @@ void loop() {
   note(0, 0);
 
   playerUpdate(p);
-  
+
   if (theBullet.enabled) {
     for (uint_fast8_t i = 0; i < INVADERS_COUNT; ++i) {
       if (!invaders[i].isDead && isColliding(&theBullet.sprite, &invaders[i].sprite)) {
@@ -224,10 +99,10 @@ void loop() {
   }
 
   // DEBUG
-  debugDisplayInt(invaderXMoveCount, 0, 50);
-  
+  //debugDisplayInt(invaderXMoveCount, 0, 50);
+
   //debugDisplayInt(freeMemory(), 0, 50);
-  debugDisplayInt(timeToWait, 55, 120);
+  //debugDisplayInt(timeToWait, 55, 120);
   //uint_fast16_t padPinValue = analogRead(PIN_DPAD);
   //debugDisplayInt(padPinValue, 40, 0);
   //debugDisplayInt(digitalRead(PIN_BUTTON_A), 70, 0);
